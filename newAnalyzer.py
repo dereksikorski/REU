698338a65b14895cc75d9f7b577d9174225494ef
dmsikors <dmsikors@iu.edu>
2022-07-13 15:59:57 -0500

    Rough Continuum fit

--- newAnalyzer.py
+++ newAnalyzer.py
@@ -1,6 +1,7 @@
 ## This file is analyzing CQ Spectra ##
 
 ## Imports
+from re import X
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize, signal, ndimage
@@ -107,10 +108,38 @@ class CQ:
 
 
         ## Fit Gaussians
-        self.fitGauss()
+        self.findGauss()
+
+        ## Roughly fit Continuum
+        self.roughContFit()
 
         ## Fit Continuum
         self.fitCont()
+
+
+
+
+    def DopplerShift(self, wavelength_data, redshift_value):
+        '''
+        INPUTS:
+
+            - wavelength_data (array)   :   Array of wavelengths
+            - redshift_value (float)    :   Value of redshift to be applied to the wavelength array
+
+        OUTPUT:
+
+            - (array)  :  Wavelength array after the redshift has been applied
+
+        This method helps "plotData" and "fitData" and is meant to shift the given data based on a given redshift value
+        '''
+        ## Apply a doppler shift to the array of wavelenghts based on the redshift value
+
+        doppler_shift = lambda wl: wl / (1 + redshift_value)
+
+        new_wavelength = list(map(doppler_shift, wavelength_data))
+
+        return new_wavelength   # (array)  -->  The new array of wavelenghts after the doppler shift
+
 
 
 
@@ -165,26 +194,136 @@ class CQ:
 
         
 
-    def DopplerShift(self, wavelength_data, redshift_value):
-        '''
-        INPUTS:
 
-            - wavelength_data (array)   :   Array of wavelengths
-            - redshift_value (float)    :   Value of redshift to be applied to the wavelength array
 
-        OUTPUT:
 
-            - (array)  :  Wavelength array after the redshift has been applied
 
-        This method helps "plotData" and "fitData" and is meant to shift the given data based on a given redshift value
-        '''
-        ## Apply a doppler shift to the array of wavelenghts based on the redshift value
 
-        doppler_shift = lambda wl: wl / (1 + redshift_value)
+    def findGauss(self):
+        """
+        Given the peak locations from 'self.findPeaks', identify data points used for Gaussian fitting
+        """
 
-        new_wavelength = list(map(doppler_shift, wavelength_data))
+        self.gaus_inds = []      # (list)  -->  Fill with lists of the gaussian indices 
 
-        return new_wavelength   # (array)  -->  The new array of wavelenghts after the doppler shift
+        ## Loop through each peak in the emission spectrum
+        for i, p_ind in enumerate(self.peak_ind):
+            half_width = signal.peak_widths(self.spectrum[:,1], [p_ind], rel_height=0.5)[0][0]   /2     # (array) --> half width of peaks
+            # MgII and H-Beta lines
+            if i in (0,1):
+                # append a smaller array of shape --> [ [index-1,....,index-n] ]  of the gaussian
+                small_width = int(2.5*half_width)   # Gives the half width of the smaller gaussian in terms of indices
+                large_width = int(7.5*half_width)   # Width of larger gaussian, in terms of indices (i.e. list of indices, not wavelengths!)
+                small_list = list(range(p_ind-small_width, p_ind+small_width+1,1))
+                large_list = list(range(p_ind-large_width,p_ind-small_width+1,1))
+                large_list = list(range(p_ind-large_width, p_ind-small_width+1)) + list(range(p_ind+small_width, p_ind+large_width+1))
+                self.gaus_inds.append( small_list )
+                self.gaus_inds.append(large_list)
+
+            # OIII line
+            elif i == 2:
+                small_width = int(2.5*half_width)   # Gives the half width of the smaller gaussian in terms of indices
+                small_list = list(range(p_ind-small_width, p_ind+small_width+1,1))
+                self.gaus_inds.append( small_list )
+            
+        ## Plotting the points marked for Gaussian fitting:
+        if self.plotSetting == "All":
+            plt.plot(self.spectrum[:,0], self.spectrum[:,1])
+            for c, idx_list in enumerate(self.gaus_inds):
+                wl,f = [self.spectrum[:,0][i] for i in idx_list], [self.spectrum[:,1][i] for i in idx_list]
+                if (c % 2)==0:
+                    plt.plot(wl,f,'g.')
+                else:
+                    plt.plot(wl,f,'m.')
+            plt.show()
+
+
+
+
+
+
+    def roughContFit(self):
+        """
+        Provides a rough fit of the continuum after removing the Gaussians marked in 'self.findGauss'
+        """
+        ## Trim data exclude marked peaks:
+        peak_pts = []
+        for gauss in self.gaus_inds:
+            for ind in gauss:
+                peak_pts.append(ind)
+        cont = np.delete(self.spectrum, peak_pts, axis = 0)
+
+        ## Plot trimmed spectrum:
+        if self.plotSetting == 'All':
+            plt.plot(cont[:,0],cont[:,1],'b.')
+            plt.show()
+        
+
+        ## Fit the curve with multiple power laws and store the error arrays:
+        fit_errors = []
+        params = []
+
+
+        for ind, wl in enumerate(cont[:,0]):               # go from 50th --> -50th so that there are enough points to fit on either side of break
+            if (ind<500) == False and (ind> (len(cont[:,0])-500) ) ==False:
+                self.wlBreak = wl
+
+                w1, w2 = cont[:,0][0:ind]  , cont[:,0][ind:len(cont[:,0])]                   # wl and flux to fit on left side of the wavelength break
+                f1, f2 = cont[:,1][0:ind]  , cont[:,1][ind:len(cont[:,1])]  
+
+                # Functions take flux/wl rather than flux:
+                y1, y2 = f1/w1, f2/w2
+
+                
+                # wl to fit on the right side of the wavelength break
+
+                p1_pars, p1_err = optimize.curve_fit(self.power1, w1, y1, p0=[1700, -0.6])
+
+                self.temp_coeff, self.temp_power = p1_pars[0], p1_pars[1]
+
+                p2_pars, p2_err = optimize.curve_fit(self.power2, w2, y2, p0=[-1])
+
+                fit1 = w1*self.power1(w1, p1_pars[0], p1_pars[1])
+                fit2 = w2*self.power2(w2, p2_pars[0])
+
+                err = np.sum( (fit1-f1)**2 / fit1 ) + np.sum( (fit2-f2)**2 / fit2)
+
+                params.append([ind, p1_pars,p2_pars, wl])
+                fit_errors.append(err)
+
+        ## Plot best fit
+        min_ind = fit_errors.index(min(fit_errors))
+        best_stats = params[min_ind]
+
+        w1b, w2b = self.spectrum[:,0][0:best_stats[0]]  , self.spectrum[:,0][best_stats[0]:len(self.spectrum[:,0])] 
+        self.wlBreak = best_stats[3]
+        plt.plot(self.spectrum[:,0], self.spectrum[:,1])
+        plt.plot(w1b, w1b*self.power1(w1b, best_stats[1][0], best_stats[1][1]), 'r.' )
+        plt.plot(w2b, w2b*self.power2(w2b, best_stats[2][0]), 'r.')
+        plt.show()
+        
+
+        
+
+            
+        
+
+
+
+
+    def power1(self, w, a,b ):
+        """
+        Fit the continuum with a power low with a coefficient that is free to vary
+        """
+        return a*w**(-b-2)
+
+    def power2(self,w, b):
+        """
+        Fit continuum, with a power law that does not have a coefficient that is free to vary
+        """
+        return  self.wlBreak**(b-self.temp_power)*(self.temp_coeff)*w**(-b-2)
+
+
 
 
     def fitGauss(self):
@@ -300,6 +439,7 @@ class CQ:
 
 
 if __name__ == "__main__":
+
 
     test = CQ()
 
